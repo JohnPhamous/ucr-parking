4f1080ea814fc0835bd545f05df9878a2c637a2a
Aaroh Mankad <aarohmankad@gmail.com>
2018-11-11 18:10:31 -0800

    link to google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -22,8 +22,20 @@ export default class Map extends Component {
           bootstrapURLKeys={{ key: KEY }}
           defaultCenter={this.props.location}
           defaultZoom={15}
-          onClick={() => console.log('hi')}
-        />
+          onClick={() =>
+            (window.location.href = `https://www.google.com/maps/search/?api=1&query=${
+              this.props.location.lat
+            },${this.props.location.lng}`)
+          }
+        >
+          <img
+            src="https://i.imgur.com/yK3IZHv.png"
+            lat={this.props.location.lat}
+            lng={this.props.location.lng}
+            height="28px"
+            alt=""
+          />
+        </GoogleMapReact>
       </MapContainer>
     )
   }
